db7c88d555993f728db1745ea6f8e67786383ecd
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2024-11-21 19:36:22 +0100

    REFACTOR: simplify div_formating filter by cleaning up the code

--- _extensions/novel-pdf/filters/div_formating.lua
+++ _extensions/novel-pdf/filters/div_formating.lua
@@ -17,7 +17,7 @@ if FORMAT:match 'latex' then
 			for i = 1, #blocks, 1 do
 				if blocks[i].t == "Para" then
 					blocks[i].content:insert(1, pandoc.RawInline('latex', [[\FirstLine{]]))
-					blocks[i].content:insert(pandoc.RawInline('latex', "}"))
+					blocks[i].content:insert(pandoc.RawInline('latex', [[}]]))
 					break
 				end
 			end
@@ -26,8 +26,9 @@ if FORMAT:match 'latex' then
 	}
 
 
-	-- Mark first letter of first paragraph for first line emphasis with bigmaj
-	local mark_first_letter_for_bigmaj = {
+	-- returns a filter that enclose the first letter of the first paragraph in a SPAN with classes
+	-- classes must be one string with space between each class (without the leading point)
+	local function filter_to_enclose_first_letter_in_span(classes) return {
 		Blocks = function(blocks)
 			-- Let's find the first para...
 			for i = 1, #blocks, 1 do
@@ -43,7 +44,7 @@ if FORMAT:match 'latex' then
 
 									inlines:remove(i)
 									inlines:insert(i, rest)
-									inlines:insert(i, pandoc.Span(first_letter, {class="firstlettermaj"}))
+									inlines:insert(i, pandoc.Span(first_letter, {class=classes}))
 									break
 								end
 							end -- for inlines
@@ -57,66 +58,11 @@ if FORMAT:match 'latex' then
 			return blocks
 		end
 	}
-
-
-	-- Mark first letter of first paragraph for first line emphasiswith dropcap
-	local mark_first_letter_for_dropcap = {
-		Blocks = function(blocks)
-			-- Let's find the first para...
-			for i = 1, #blocks, 1 do
-				if blocks[i].t == "Para" then
-					blocks[i] = blocks[i]:walk {
-						Inlines = function(inlines)
-							-- ...and in this para, let's find the first word and modify it
-							for i = 1, #inlines, 1 do
-								if inlines[i].t == "Str" then
-									first_word = pandoc.utils.stringify(inlines[i])
-									first_letter = first_word:sub(1, 1)
-									rest = first_word:sub(2)
-
-									inlines:remove(i)
-									inlines:insert(i, rest)
-									inlines:insert(i, pandoc.Span(first_letter, {class="dropcap"}))
-									break
-								end
-							end -- for inlines
-
-							return inlines
-						end
-					}
-					break
-				end
-			end -- for blocks
-			return blocks
-		end
-	}
-
-	-- Make paragraphs small caps if needed
-	local make_changes_to_para = {
-		Para = function(para)
-			local before_para = {}
-			local after_para = {}
-
-			-- As \scshape does not work we need to apply \textsc para by para
-			if para_need_smallcaps then
-				table.insert(before_para, [[\textsc{]])
-				table.insert(after_para, "}")
-			end
-
-			return {
-				{pandoc.RawInline('latex', table.concat(before_para, ""))}
-				.. para.content
-				.. {pandoc.RawInline('latex', table.concat(after_para, ""))}
-			}
-		end
-	}
+	end
 
 
 	-- filter for Div
-	function add_formating_to_div(div)
-		local all_latex_before = {}  -- to store opening latex elements (same order as attributes)
-		local all_latex_after = {}  -- to store closing latex elements (in reversed order of attributes)
-
+	local function add_formating_to_div(div)
 		-- iterate over all the classes and attributes
 		local classes_and_attrs = {}
 		for _, class_name in ipairs(div.classes) do
@@ -129,33 +75,39 @@ if FORMAT:match 'latex' then
 		for name, value in pairs(classes_and_attrs) do
 
 			if name == "bold" then
-				table.insert(all_latex_before, [[\begin{bfseries}]])
-				table.insert(all_latex_after, 1, [[\end{bfseries}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{bfseries}]]))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{bfseries}]]))
 			elseif name == "italic" then
-				table.insert(all_latex_before, [[\begin{itshape}]])
-				table.insert(all_latex_after, 1, [[\end{itshape}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{itshape}]]))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{itshape}]]))
 			elseif name == "strikethrough" then
-				table.insert(all_latex_before, [[\st{]])
-				table.insert(all_latex_after, 1, "}")
+				div.content:insert(1, pandoc.RawBlock('latex', [[\st{]]))
+				div.content:insert(pandoc.RawBlock('latex', [[}]]))
 			elseif name == "smallcaps" then
 				-- BUG just using \scshape does not work (no error but no actual effect)
 				-- So we add \textsc to each paragraphs
-				para_need_smallcaps = true
+				div = div:walk {
+					Para = function(para)
+						para.content:insert(1, pandoc.RawInline('latex', [[\textsc{]]))
+						para.content:insert(pandoc.RawInline('latex', [[}]]))
+						return para
+					end
+				}
 			elseif name == "monospace" then
-				table.insert(all_latex_before, [[\begin{ttfamily}]])
-				table.insert(all_latex_after, 1, [[\end{ttfamily}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{ttfamily}]]))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{ttfamily}]]))
 
 				-- We had ragged right command since it's not possible to have monospaced justified text
 				-- since LaTeX can't adjust spaces in any way. And also to respect the alignment of
 				-- letters from line to line justified paragraph
-				table.insert(all_latex_before, [[\begin{flushleft}]])
-				table.insert(all_latex_after, 1, [[\end{flushleft}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{flushleft}]]))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{flushleft}]]))
 			elseif name == "noparindent" then
 				-- Retreive value from meta
 				local parindent = pandoc.utils.stringify(from_meta.page_layout.parindent)
 
-				table.insert(all_latex_before, [[\setlength{\parindent}{0em}]])
-				table.insert(all_latex_after, 1, [[\setlength{\parindent}{]] .. parindent .. [[}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\setlength{\parindent}{0em}]]))
+				div.content:insert(pandoc.RawBlock('latex', [[\setlength{\parindent}{%(indent)s}]] % {indent=parindent}))
 			elseif name == "parskip" then
 				-- Retreive value from attribute
 				local local_parskip = pandoc.utils.stringify(value)
@@ -163,95 +115,83 @@ if FORMAT:match 'latex' then
 				-- Retreive value from meta
 				local global_parskip = pandoc.utils.stringify(from_meta.page_layout.parskip)
 
-				table.insert(all_latex_before, [[\setlength{\parskip}{]] .. local_parskip .. [[}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\setlength{\parskip}{%(skip)s}]] % {skip=local_parskip}))
 				-- Since we have a space before the first paragraphe we insert a \null after
 				-- the last one to also have a space after it (it's more symetrical)
-				table.insert(all_latex_after, 1, [[\null\setlength{\parskip}{]] .. global_parskip .. [[}]])
+				div.content:insert(pandoc.RawBlock('latex', [[\null\setlength{\parskip}{%(skip)s}]] % {skip=global_parskip}))
 			elseif name == "vfill" then
 				-- Retreive value from attribute
 				local vfill = pandoc.utils.stringify(value)
 
 				if vfill == "before" then
-					table.insert(all_latex_before, [[\null\vfill]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\null\vfill]]))
 				elseif vfill == "after" then
-					table.insert(all_latex_after, 1, [[\vfill\null]])
+					div.content:insert(pandoc.RawBlock('latex', [[\vfill\null]]))
 				elseif vfill == "both" then
-					table.insert(all_latex_before, [[\null\vfill]])
-					table.insert(all_latex_after, 1, [[\vfill\null]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\null\vfill]]))
+					div.content:insert(pandoc.RawBlock('latex', [[\vfill\null]]))
 				else
 					error("vfill div attribute only possible values are before, after and both, but has value: %(val)s." % {val=vfill})
 				end
 			elseif name == "scale" then
-				table.insert(all_latex_before, [[\begin{parascale}[%(scale)s] ]] % {scale=value})
-				table.insert(all_latex_after, 1, [[\end{parascale}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{parascale}[%(scale)s] ]] % {scale=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{parascale}]]))
 			-- TODO: make number of lines the default unit if none provided
 			elseif name == "vspace_before" then
-				table.insert(all_latex_before, [[\vspace*{%(space)s}]] % {space=value})
+				div.content:insert(1, pandoc.RawBlock('latex', [[\vspace*{%(space)s}]] % {space=value}))
 			elseif name == "vspace_after" then
-				table.insert(all_latex_after, 1, [[\vspace*{%(space)s}]] % {space=value})
+				div.content:insert(pandoc.RawBlock('latex', [[\vspace*{%(space)s}]] % {space=value}))
 			elseif name == "vspace_both" then
-				table.insert(all_latex_before, [[\vspace*{%(space)s}]] % {space=value})
-				table.insert(all_latex_after, 1, [[\vspace*{%(space)s}]] % {space=value})
+				div.content:insert(1, pandoc.RawBlock('latex', [[\vspace*{%(space)s}]] % {space=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\vspace*{%(space)s}]] % {space=value}))
 			elseif name == "lines_before" then
-				table.insert(all_latex_before, [[\vspace*{%(nb)s\nbs}]] % {nb=value})
+				div.content:insert(1, pandoc.RawBlock('latex', [[\vspace*{%(nb)s\nbs}]] % {nb=value}))
 			elseif name == "lines_after" then
-				table.insert(all_latex_after, 1, [[\vspace*{%(nb)s\nbs}]] % {nb=value})
+				div.content:insert(pandoc.RawBlock('latex', [[\vspace*{%(nb)s\nbs}]] % {nb=value}))
 			elseif name == "lines_both" then
-				table.insert(all_latex_before, [[\vspace*{%(nb)s\nbs}]] % {nb=value})
-				table.insert(all_latex_after, 1, [[\vspace*{%(nb)s\nbs}]] % {nb=value})
+				div.content:insert(1, pandoc.RawBlock('latex', [[\vspace*{%(nb)s\nbs}]] % {nb=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\vspace*{%(nb)s\nbs}]] % {nb=value}))
 			elseif name == "margin_left" then
-				table.insert(all_latex_before, [[\begin{adjustwidth}{%(margin)s}{0em}]] % {margin=value})
-				table.insert(all_latex_after, 1, [[\end{adjustwidth}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{adjustwidth}{%(margin)s}{0em}]] % {margin=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{adjustwidth}]]))
 			elseif name == "margin_right" then
-				table.insert(all_latex_before, [[\begin{adjustwidth}{0em}{%(margin)s}]] % {margin=value})
-				table.insert(all_latex_after, 1, [[\end{adjustwidth}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{adjustwidth}{0em}{%(margin)s}]] % {margin=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{adjustwidth}]]))
 			elseif name == "margin_both" then
-				table.insert(all_latex_before, [[\begin{adjustwidth}{%(margin)s}{%(margin)s}]] % {margin=value})
-				table.insert(all_latex_after, 1, [[\end{adjustwidth}]])
+				div.content:insert(1, pandoc.RawBlock('latex', [[\begin{adjustwidth}{%(margin)s}{%(margin)s}]] % {margin=value}))
+				div.content:insert(pandoc.RawBlock('latex', [[\end{adjustwidth}]]))
 			elseif name == "align" then
 				if value == "left" then
-					table.insert(all_latex_before, [[\begin{FlushLeft}]])
-					table.insert(all_latex_after, 1, [[\end{FlushLeft}]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\begin{FlushLeft}]]))
+					div.content:insert(pandoc.RawBlock('latex', [[\end{FlushLeft}]]))
 				elseif value == "right" then
-					table.insert(all_latex_before, [[\begin{FlushRight}]])
-					table.insert(all_latex_after, 1, [[\end{FlushRight}]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\begin{FlushRight}]]))
+					div.content:insert(pandoc.RawBlock('latex', [[\end{FlushRight}]]))
 				elseif value == "center" then
-					table.insert(all_latex_before, [[\begin{Center}]])
-					table.insert(all_latex_after, 1, [[\end{Center}]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\begin{Center}]]))
+					div.content:insert(pandoc.RawBlock('latex', [[\end{Center}]]))
 				elseif value == "justify" then
-					table.insert(all_latex_before, [[\begin{justify}]])
-					table.insert(all_latex_after, 1, [[\end{justify}]])
+					div.content:insert(1, pandoc.RawBlock('latex', [[\begin{justify}]]))
+					div.content:insert(pandoc.RawBlock('latex', [[\end{justify}]]))
 				else
 					error("align attribute with value '%(attr_val)s' but the only possible values are left, right, center and justify." % {attr_val=value})
 				end
 			elseif name == "firstlinesc" then
 				div = div:walk(mark_first_paragraph)
 			elseif name == "firstlettermaj" then
-				div = div:walk(mark_first_letter_for_bigmaj)
+				div = div:walk(filter_to_enclose_first_letter_in_span("firstlettermaj"))
 			elseif name == "dropcap" then
-				div = div:walk(mark_first_letter_for_dropcap)
+				div = div:walk(filter_to_enclose_first_letter_in_span("dropcap"))
 			end
 
 		end  -- for classes_and_attrs
 
-		-- TODO replace this by a local walk in the big for-if above
-		-- Some changes need to be done for each para
-		local div = div:walk(make_changes_to_para)
-		para_need_smallcaps = false
-
-
-		return {
-			pandoc.RawBlock('latex', table.concat(all_latex_before, "\n")),
-			div,
-			pandoc.RawBlock('latex', table.concat(all_latex_after, "\n")),
-		}
+		-- Return the content of the modified div (the div itself is not usefull in LaTeX)
+		return div.content
 	end
 
 	-- filter for Span
-	function add_formating_to_span(span)
-		local all_latex_before = {}  -- to store opening latex elements (same order as attributes)
-		local all_latex_after = {}  -- to store closing latex elements (in reversed order of attributes)
-
+	local function add_formating_to_span(span)
 		-- iterate over all the classes and attributes
 		local classes_and_attrs = {}
 		for _, class_name in ipairs(span.classes) do
@@ -263,40 +203,40 @@ if FORMAT:match 'latex' then
 
 		for name, value in pairs(classes_and_attrs) do
 			if name == "bold" then
-				table.insert(all_latex_before, [[\textbf{]])
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\textbf{]]))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "italic" then
-				table.insert(all_latex_before, [[\textit{]])
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\textit{]]))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "strikethrough" then
-				table.insert(all_latex_before, [[\st{]])
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\st{]]))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "smallcaps" then
 				-- don't need to do anything since pandoc automaticaly add \textsc{...} around span with smallcaps class
 			elseif name == "monospace" then
-				table.insert(all_latex_before, [[\texttt{]])
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\texttt{]]))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "scale" then
-				table.insert(all_latex_before, [[\charscale[%(params)s]{]] % {params=value})
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\charscale[%(params)s]{]] % {params=value}))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "hspace_before" then
-				table.insert(all_latex_before, [[\hspace*{%(space)s}]] % {space=value})
+				span.content:insert(1, pandoc.RawInline('latex', [[\hspace*{%(space)s}]] % {space=value}))
 			elseif name == "hspace_after" then
-				table.insert(all_latex_after, 1, [[\hspace*{%(space)s}]] % {space=value})
+				span.content:insert(pandoc.RawInline('latex', [[\hspace*{%(space)s}]] % {space=value}))
 			elseif name == "hspace_both" then
-				table.insert(all_latex_before, [[\hspace*{%(space)s}]] % {space=value})
-				table.insert(all_latex_after, 1, [[\vspace*{%(space)s}]] % {space=value})
+				span.content:insert(1, pandoc.RawInline('latex', [[\hspace*{%(space)s}]] % {space=value}))
+				span.content:insert(pandoc.RawInline('latex', [[\vspace*{%(space)s}]] % {space=value}))
 			elseif name == "phantom" then
-				table.insert(all_latex_before, [[\phantom{]])
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[\phantom{]]))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "hfill" then
 				if value == "before" then
-					table.insert(all_latex_before, [[\strut\hfill]])
+					span.content:insert(1, pandoc.RawInline('latex', [[\strut\hfill]]))
 				elseif value == "after" then
-					table.insert(all_latex_after, 1, [[\strut\hfill]])
+					span.content:insert(pandoc.RawInline('latex', [[\strut\hfill]]))
 				elseif value == "both" then
-					table.insert(all_latex_before, [[\strut\hfill]])
-					table.insert(all_latex_after, 1, [[\strut\hfill]])
+					span.content:insert(1, pandoc.RawInline('latex', [[\strut\hfill]]))
+					span.content:insert(pandoc.RawInline('latex', [[\strut\hfill]]))
 				else
 					error("hfill attribute with value '%(attr_val)s' but the only possible values are before, after and both." % {attr_val=value})
 				end
@@ -306,33 +246,26 @@ if FORMAT:match 'latex' then
 					utils.table_contains(allowed_colors, value),
 					"Invalid color %(color)s. Accepted colors are: black, gray1, gray2, gray3, gray4, gray5, gray6, gray7, gray8, gray9, white" % {color=value}
 				)
-				table.insert(all_latex_before, [[{\color{%(color)s}]] % {color=value})
-				table.insert(all_latex_after, 1, "}")
+				span.content:insert(1, pandoc.RawInline('latex', [[{\color{%(color)s}]] % {color=value}))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			elseif name == "firstlettermaj" then
 				-- Retreive value from meta
 				local scale = pandoc.utils.stringify(from_meta.chapters.beginning.bigmaj.scale)
 				local hspace_after = pandoc.utils.stringify(from_meta.chapters.beginning.bigmaj.hspace_after)
 
-				table.insert(all_latex_before, [[\charscale[%(scale)s]{\firstletterfont ]] % {scale=scale})
-				table.insert(all_latex_after, 1, [[}\hspace{%(space)s}]] % {space=hspace_after})
+				span.content:insert(1, pandoc.RawInline('latex', [[\charscale[%(scale)s]{\firstletterfont ]] % {scale=scale}))
+				span.content:insert(pandoc.RawInline('latex', [[}\hspace{%(space)s}]] % {space=hspace_after}))
 			elseif name == "dropcap" then
 				-- Retreive value from meta
 				local lines = pandoc.utils.stringify(from_meta.chapters.beginning.dropcap.lines)
 
-				table.insert(all_latex_before, [[\dropcap[lines=%(lines)s]{]] % {lines=lines})
-				table.insert(all_latex_after, 1, [[}]])
+				span.content:insert(1, pandoc.RawInline('latex', [[\dropcap[lines=%(lines)s]{]] % {lines=lines}))
+				span.content:insert(pandoc.RawInline('latex', [[}]]))
 			end
 		end
 
-		-- Get the content
-		local content = span:walk()
-
-		-- Encapsulate the content and return it
-		return {
-			pandoc.RawInline('latex', table.concat(all_latex_before, "")),
-			content,
-			pandoc.RawInline('latex', table.concat(all_latex_after, "")),
-		}
+		-- Return the content of the modified SPAN (the DIV itself is not usefull in LaTeX)
+		return span.content
 	end
 
 	-- Set what function will be called on what kind of Pandoc element and in which order
