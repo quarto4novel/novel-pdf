bf59e8bc2b8b4c1701033490c6ee7adee002f8a7
Pierre-Yves Martin <pym.aldebaran@gmail.com>
2024-11-24 13:16:54 +0100

    REFACTOR: cleanup div, span and chapter filters in order to make chapter first letter/line integration possible

--- _extensions/novel-pdf/filters/chapter_div.lua
+++ _extensions/novel-pdf/filters/chapter_div.lua
@@ -6,9 +6,6 @@ local from_meta = {}
 local chap_builder = nil
 
 if FORMAT:match 'latex' then
-	function get_vspaces_from_meta(meta)
-		from_meta.chapters = meta.chapters
-	end
 
 	-- Used localy inside chapter div
 	local chapter_titles_from_header = {
@@ -39,36 +36,50 @@ if FORMAT:match 'latex' then
 		end
 	}
 
-	function chapter_start_from_div(div)
-		if utils.table_contains(div.classes, "chapter") then
-			-- We need a builder
-			chap_builder = builders.ChapterBuilder:new()
+	function chapter_start_from_div(blocks)
+		local height
+		local page_style
+		local content
+
+		for i, div in ipairs(blocks) do
+			-- Anything but div chapter, do nothing
+			if not (div.t == "Div" and utils.table_contains(div.classes, "chapter")) then goto continue end
 
 			-- Retreive attributes or get default from meta
-			local height = pandoc.utils.stringify(div.attributes.height or from_meta.chapters.header_height)
-			local page_style = pandoc.utils.stringify(div.attributes.page_style or from_meta.chapters.page_style)
+			height = pandoc.utils.stringify(div.attributes.height or from_meta.chapters.header_height)
+			page_style = pandoc.utils.stringify(div.attributes.page_style or from_meta.chapters.page_style)
+
+			-- We need a global builder so that walk can access it
+			chap_builder = builders.ChapterBuilder:new()
+
+			-- Retreive the content of the chapter after some conversion
+			-- Warning: we need the global chap_builder to already exist for this !
+			content = div:walk(chapter_titles_from_header)
 
 			-- build the chapter
 			chap_builder
 				:height(height)
 				:page_style(page_style)
-				:content_block(div:walk(chapter_titles_from_header))
+				:content_block(content)
 
-			return chap_builder:build()
+			-- Replace the block by what we built
+			blocks[i] = chap_builder:build()
+
+			::continue::
+			chap_builder = nil
 		end
 
-		-- if the div doesn't have the correct class we are not touching it
-		return nil
+		return blocks
 	end
 
 	-- Set what function will be called on what kind of Pandoc element and in which order
 	-- See: https://pandoc.org/lua-filters.html#typewise-traversal
 	return {
 		{
-			Meta = get_vspaces_from_meta
+			Meta = function(meta) from_meta = meta end
 		},
 		{
-			Div = chapter_start_from_div
+			Blocks = chapter_start_from_div
 		}
 	}
 end
--- _extensions/novel-pdf/filters/headings.lua
+++ _extensions/novel-pdf/filters/headings.lua
@@ -10,115 +10,146 @@ local MATTER <const> = {
 
 if FORMAT:match 'latex' then
 	local from_meta = {}
-	local current_matter = MATTER.NOTHING
 
-	function get_param_from_meta(meta)
-		from_meta.chapters = meta.chapters
-		from_meta.quickchapters = meta.quickchapters
-		from_meta.scenebreaks = meta.scenebreaks
-		from_meta.parts = meta.parts
+	--- return the LaTeX representation of a front/body/back matter that should
+	-- replace the header 1 element
+	-- @param header the header from which to generate the matter from
+	-- @return LaTeX Block to replace the header with
+	-- @return int representing the kind of matter (NOTHING, FRONT, BODY or BACK)
+	local function _matter_from_header_1(header)
+		local title = pandoc.utils.stringify(header.content)
+
+		if title == "Front matter" then
+			print("Front matter start")
+			return builders.build_frontmatter(), MATTER.FRONT
+
+		elseif title == "Body matter" then
+			print("Body matter start")
+			return builders.build_mainmatter(), MATTER.BODY
+
+		elseif title == "Back matter" then
+			print("Back matter start")
+			return builders.build_backmatter(), MATTER.BACK
+		else
+			error("Level 1 heading '# %(title)s' found but the only possible title are Front matter, Body matter and Back matter" % {title=title})
+		end
 	end
 
 
-	local function structure_from_headers(header)
+	local function _frontback_matter_subpart_from_header_2(header)
 		local title = pandoc.utils.stringify(header.content)
 
-		if header.level == 1 then
-			if title == "Front matter" then
-				print("Front matter start")
-				current_matter = MATTER.FRONT
-				return builders.build_frontmatter()
+		if utils.table_contains(header.classes, "chapterlike") then
+			-- TODO add specific default config for chapterlike
+			-- Retreive attributes or get default from meta
+			local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.chapters.title.lines_before)
+			local height = pandoc.utils.stringify(header.attributes.height or from_meta.chapters.header_height)
 
-			elseif title == "Body matter" then
-				print("Body matter start")
-				current_matter = MATTER.BODY
-				return builders.build_mainmatter()
+			-- build chapterlike
+			local chapter = builders.ChapterBuilder:new()
+				:title_inlines(header.content)
+				:lines_before_title(lines_before)
+				:height(height)
+				-- TODO: add support for chapter style
+				:build()
 
-			elseif title == "Back matter" then
-				print("Back matter start")
-				current_matter = MATTER.BACK
-				return builders.build_backmatter()
-			else
-				error("Level 1 heading '# %(title)s' found but the only possible title are Front matter, Body matter and Back matter" % {title=title})
-			end
-		end -- level 1
+			return chapter
+		else
+			-- TODO: add support for .samepage class
+			-- TODO: add support for page_style=empty attribute
+			return builders.build_frontmatter_sub(title)
+		end
+	end
 
-		if current_matter == MATTER.FRONT or current_matter == MATTER.BACK then
-			if header.level == 2 then
-				if utils.table_contains(header.classes, "chapterlike") then
-					-- TODO add specific default config for chapterlike
-					-- Retreive attributes or get default from meta
-					local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.chapters.title.lines_before)
-					local height = pandoc.utils.stringify(header.attributes.height or from_meta.chapters.header_height)
+	local function _chapter_or_part_from_header_2(header)
+		if utils.table_contains(header.classes, "part") then
+			-- Retreive attributes or get default from meta
+			local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.parts.title.lines_before)
+			local height = pandoc.utils.stringify(header.attributes.height or from_meta.parts.header_height)
+			local scale = pandoc.utils.stringify(header.attributes.scale or from_meta.parts.title.scale)
 
-					-- build chapterlike
-					local chapter = builders.ChapterBuilder:new()
-						:title_inlines(header.content)
-						:lines_before_title(lines_before)
-						:height(height)
-						-- TODO: add support for chapter style
-						:build()
+			-- Build the part
+			local part = builders.PartBuilder:new()
+				:title_inlines(header.content)
+				:title_scale(scale)
+				:lines_before_title(lines_before)
+				:height(height)
+				:build()
 
-					return chapter
-				else
-					-- TODO: add support for .samepage class
-					-- TODO: add support for page_style=empty attribute
-					return builders.build_frontmatter_sub(title)
+			return part
+		else
+			-- Retreive attributes or get default from meta
+			local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.chapters.title.lines_before)
+			local height = pandoc.utils.stringify(header.attributes.height or from_meta.chapters.header_height)
+			local page_style = pandoc.utils.stringify(header.attributes.page_style or from_meta.chapters.page_style)
+
+			local chap_builder = builders.ChapterBuilder:new()
+				:title_inlines(header.content)
+				:lines_before_title(lines_before)
+				:height(height)
+				:page_style(page_style)
+
+			return chap_builder:build()
+		end
+	end
+
+
+	local function _quickchapter_from_header_3(header)
+		-- Retreive attributes or get default from meta
+		local name_inlines = header.content
+		local line = pandoc.utils.stringify(header.attributes.line or from_meta.quickchapters.line)
+
+		return builders.build_quickchapter(name_inlines, line)
+	end
+
+
+	local function _scenebreak_from_header_4(header)
+		local title = pandoc.utils.stringify(header.content)
+
+		-- Retreive attributes or get default from meta
+		local default_break = pandoc.utils.stringify(from_meta.scenebreaks.default)
+
+		return builders.build_scenebreak(title, default_break)
+	end
+
+
+	local function structure_from_headers(blocks)
+		local current_matter = MATTER.NOTHING
+
+		for i, header in ipairs(blocks) do
+			-- Anything but header, do nothing
+			if header.t ~= "Header" then goto continue end
+
+			if header.level == 1 then
+				blocks[i], current_matter = _matter_from_header_1(header)
+			end -- level 1
+
+			if current_matter == MATTER.FRONT
+			or current_matter == MATTER.BACK then
+				if header.level == 2 then
+					blocks[i] = _frontback_matter_subpart_from_header_2(header)
+				end
+			elseif current_matter == MATTER.BODY then
+				if header.level == 2 then
+					blocks[i] = _chapter_or_part_from_header_2(header)
+				elseif header.level == 3 then
+					blocks[i] = _quickchapter_from_header_3(header)
+				elseif header.level == 4 then
+					blocks[i] = _scenebreak_from_header_4(header)
 				end
 			end
-		elseif current_matter == MATTER.BODY then
-			if header.level == 2 then
-				if utils.table_contains(header.classes, "part") then
-					-- Retreive attributes or get default from meta
-					local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.parts.title.lines_before)
-					local height = pandoc.utils.stringify(header.attributes.height or from_meta.parts.header_height)
-					local scale = pandoc.utils.stringify(header.attributes.scale or from_meta.parts.title.scale)
+			::continue::
+		end -- for blocks
 
-					-- Build the part
-					local part = builders.PartBuilder:new()
-						:title_inlines(header.content)
-						:title_scale(scale)
-						:lines_before_title(lines_before)
-						:height(height)
-						:build()
-
-					return part
-				else
-					-- Retreive attributes or get default from meta
-					local lines_before = pandoc.utils.stringify(header.attributes.lines_before or from_meta.chapters.title.lines_before)
-					local height = pandoc.utils.stringify(header.attributes.height or from_meta.chapters.header_height)
-					local page_style = pandoc.utils.stringify(header.attributes.page_style or from_meta.chapters.page_style)
-
-					local chap_builder = builders.ChapterBuilder:new()
-						:title_inlines(header.content)
-						:lines_before_title(lines_before)
-						:height(height)
-						:page_style(page_style)
-
-					return chap_builder:build()
-				end
-			elseif header.level == 3 then
-				-- Retreive attributes or get default from meta
-				local name_inlines = header.content
-				local line = pandoc.utils.stringify(header.attributes.line or from_meta.quickchapters.line)
-
-				return builders.build_quickchapter(name_inlines, line)
-
-			elseif header.level == 4 then
-				-- Retreive attributes or get default from meta
-				local default_break = pandoc.utils.stringify(from_meta.scenebreaks.default)
-
-				return builders.build_scenebreak(title, default_break)
-			end
-		end -- current_matter
+		return blocks
 	end
 
 	return {
 		{
-			Meta = get_param_from_meta
+			Meta = function(meta) from_meta = meta end
 		},
 		{
-			Header = structure_from_headers
+			Blocks = structure_from_headers
 		}
 	}
 end
